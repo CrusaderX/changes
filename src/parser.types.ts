2fd6522ee6de0800d8318b238959169c5e75bb95
CrusaderX <thepliable2@gmail.com>
2025-04-01 21:33:54 +0200

    build

--- src/parser.types.ts
+++ src/parser.types.ts
@@ -10,12 +10,6 @@ interface IParserErrorOutput {
 
 export type IParserOutput = IParserSuccessOutput | IParserErrorOutput;
 
-export interface IGetCommit {
-  owner: string;
-  repo: string;
-  ref: string;
-}
-
 export interface IFile {
   additions: number;
   blob_url: string;
